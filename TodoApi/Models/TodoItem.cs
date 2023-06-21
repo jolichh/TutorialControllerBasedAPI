5b391088c7bec16c7acf42b6891f57d90a1b4d8b
joana.jiayun <joana.jiayun@IBLAESBARC02144>
2023-06-21 12:41:09 +0200

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }    //Id functions as the unique key in a relational database
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; } //hidden from app but admin app could choose to expose it
+}
